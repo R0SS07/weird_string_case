2f7a787d81a38f3cb852f966b42d969ac404508f
cannoniere7 <rusty.code.20@gmail.com>
2022-01-26 06:13:34 +0200

    The solution of the task

--- weird_string_case.go
+++ weird_string_case.go
@@ -1,7 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
-	toWeirdCase("abc def")
+	fmt.Println(toWeirdCase("String"))            // "StRiNg"
+	fmt.Println(toWeirdCase("Weird string case")) // "WeIrD StRiNg CaSe"
+
 }
 func toWeirdCase(str string) string {
+	arr := strings.Split(str, "")
+	res := []string{}
+	index := 0
+	for k, value := range arr {
+		if index%2.0 == 0 {
+			res = append(res, strings.ToUpper(value))
+		} else {
+			res = append(res, strings.ToLower(value))
+		}
+		if arr[k] == " " {
+			index = 0
+		} else {
+			index++
+		}
+	}
+	return strings.Join(res, "")
 }
